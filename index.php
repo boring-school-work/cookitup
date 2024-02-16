17f88e1ed7e64ce997cc511c87b79d7706700dba
David Saah <dasorange.hope@gmail.com>
2024-02-16 17:38:28 +0000

    chore: setup folder structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+
+<head>
+  <title>Cookitup - A Recipe Sharing Platform</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./assets/css/style.css" rel="stylesheet" />
+  <link href="./assets/css/output.css" rel="stylesheet" />
+</head>
+
+<body>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php", "./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
