7176ae2ab0df5e4f8ffadb762811db9e25d1122f
David Saah <dasorange.hope@gmail.com>
2024-02-20 14:28:43 +0000

    chore: update register with tailwind

--- register/index.php
+++ register/index.php
@@ -1,63 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./../assets/css/style.css" />
-    <link rel="stylesheet" href="./../assets/css/output.css" />
-    <title>Register Page</title>
-    <style>
-        body {
-            display: flex;
-            orientation:portrait;
-            
-        }
-        #register-image {
-            max-width: 35%; 
-            height: auto;
-            align-items: flex-start;
-        }
-
-        #form-content {
-            display: flex;
-            flex-direction: column;
-            align-content: end;
-            width: 35%;
-            height: auto;
-            padding-left: 7%;
-            padding-right: 7%;
-            padding-top: 5%;
-            padding-bottom: 5%;
-            background-color: #dec3a4;
-        }
-        
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./../assets/css/style.css" />
+  <link rel="stylesheet" href="./../assets/css/output.css" />
+  <title>Cookitup | Register</title>
 </head>
-<body>
-    <!-- Register Page -->
-    <img src="./../assets/images/register-image.jpg" alt="Food image" id="register-image">
-    <form id="form-content">
-        <h1 style="font-size:x-large" >Create an account</h1>
-        <p>Already Registered? <a href="login.html">Login</a></p> 
-        <hr>
-        <br>
 
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname" placeholder=" John "><br>
-
-        <label for="surname">Surname:</label>
-        <input type="text" id="surname" name="surname" placeholder=" Mwangi "><br>
-
-        <label for="email">Email Address:</label>
-        <input type="email" id="email" name="email" placeholder=" johnmwangi@gmail.com " autocomplete="off"><br>
-
-        <label for="birthday">Date of Birth:</label>
-        <input type="date" id="birthday" name="birthday"><br>
-
-        <label for="pwd">Password:</label>
-        <input type="password" id="pwd" name="pwd" placeholder=" ******** "><br>
-
-        <input type="button" onclick="alert('Successfully registered!')" value="Register!" class="ring ring-purple-500" style="background-color: #bb86fc;">
-    </form>
+<body class="bg-[#dec3a4] font-mono">
+  <div class="flex justify-center">
+    <div class="w-1/2">
+      <img class="w-full object-cover h-screen" src="./../assets/images/register-image.jpg" alt="Food image" id="register-image">
+    </div>
+    <div class="w-1/2 flex flex-col items-center justify-center text-center">
+      <div class="mb-12 border-b-2 w-full border-b-orange-800">
+        <h1 class="text-4xl">Create An Account</h1>
+        <div class="pb-4">Already Registered? <a class="text-purple-700 font-semibold" href="./../login/">Login</a></div>
+      </div>
+      <form id="register" name="register" class="w-1/2">
+        <div class="flex flex-col mb-4">
+          <label for="fname" class="text-left text-lg">First name:</label>
+          <input type="text" id="fname" name="fname" placeholder=" John " class="px-10 py-3 rounded-sm">
+        </div>
+        <div class="flex flex-col mb-4">
+          <label for="lname" class="text-left text-lg">Surname:</label>
+          <input type="text" id="lname" name="lname" placeholder=" Mwangi " class="px-10 py-3 rounded-sm">
+        </div>
+        <div class="flex flex-col mb-4">
+          <label for="email" class="text-left text-lg">Email Address:</label>
+          <input type="email" id="email" name="email" placeholder=" johnmwangi@gmail.com " class="px-10 py-3 rounded-sm" autocomplete="off">
+        </div>
+        <div class="flex flex-col mb-4">
+          <label for="passwd" class="text-left text-lg">Password:</label>
+          <input type="password" id="passwd" name="passwd" placeholder=" ******** " class="px-10 py-3 rounded-sm">
+        </div>
+        <input type="submit" value="SIGN UP" class="text-white font-semibold bg-purple-400 px-10 py-2 rounded-md mt-4 cursor-pointer">
+      </form>
+    </div>
+  </div>
 </body>
+
 </html>
