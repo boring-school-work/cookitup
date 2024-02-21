0186eca1e580079ad2da965d7b9d5c0a720c212d
MrSanguini <papadadson@gmail.com>
2024-02-21 22:09:06 +0000

    Beginning JS

---
+++ script.js
@@ -0,0 +1,4 @@
+//Featured section
+const featuredImage = document.querySelector("#featured-img");
+const featuredText = document.querySelector("#feature-text");
+const featuredName = document.querySelector("#featured-name");
