b1145b64def65292571bf7fe39a2804cb180e5a4
David Saah <dasorange.hope@gmail.com>
2024-03-07 21:56:50 +0000

    feat: add form validation

--- register/index.php
+++ register/index.php
@@ -22,19 +22,19 @@
       <form id="register" name="register" class="w-1/2">
         <div class="flex flex-col mb-4">
           <label for="fname" class="text-left text-lg">First name:</label>
-          <input type="text" id="fname" name="fname" placeholder=" John " class="px-10 py-3 rounded-sm">
+          <input type="text" id="fname" name="fname" placeholder=" John " class="px-10 py-3 rounded-sm" required>
         </div>
         <div class="flex flex-col mb-4">
           <label for="lname" class="text-left text-lg">Surname:</label>
-          <input type="text" id="lname" name="lname" placeholder=" Mwangi " class="px-10 py-3 rounded-sm">
+          <input type="text" id="lname" name="lname" placeholder=" Mwangi " class="px-10 py-3 rounded-sm" required>
         </div>
         <div class="flex flex-col mb-4">
           <label for="email" class="text-left text-lg">Email Address:</label>
-          <input type="email" id="email" name="email" placeholder=" johnmwangi@gmail.com " class="px-10 py-3 rounded-sm" autocomplete="off">
+          <input type="email" id="email" name="email" placeholder=" johnmwangi@gmail.com " pattern="^[a-z._\-0-9]*[@][a-z]*.(?:...com)$" class="px-10 py-3 rounded-sm" autocomplete="off" required>
         </div>
         <div class="flex flex-col mb-4">
           <label for="passwd" class="text-left text-lg">Password:</label>
-          <input type="password" id="passwd" name="passwd" placeholder=" ******** " class="px-10 py-3 rounded-sm">
+          <input type="password" id="passwd" name="passwd" placeholder=" ******** " class="px-10 py-3 rounded-sm" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
         </div>
         <input type="submit" value="SIGN UP" class="text-white font-semibold bg-purple-400 px-10 py-2 rounded-md mt-4 cursor-pointer">
       </form>
