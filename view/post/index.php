82030943275c4964919d4207bc449daf67da2877
David Saah <dasorange.hope@gmail.com>
2024-03-09 20:20:51 +0000

    feat: add view recipe post page

---
+++ view/post/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../assets/css/style.css" />
+  <link rel="stylesheet" href="../../assets/css/output.css" />
+  <title>Cookitup</title>
+</head>
+
+<body class="bg-[#f1e8d5]">
+
+  <!-- PLACE NAVBAR HERE -->
+
+  <!-- Header for Recipe -->
+  <div class="flex justify-center items-start gap-x-5">
+    <div>
+      <img src="../../assets/images/register-image.jpg" class="object-cover w-96 h-96" alt="recipe image" />
+    </div>
+    <div class="flex flex-col justify-between w-auto h-96">
+      <div class="grow mt-20">
+        <h3 class="capitalize font-semibold text-3xl text-center mb-3">title of recipe</h3>
+        <p class="">A dish of fried plantain and baked beans</p>
+      </div>
+      <div>
+        <p class="italic font-semibold pb-5">by: John Doe</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Ingredients & Direction -->
+  <div class="mt-8 flex justify-center gap-x-10 px-52">
+    <div class="flex flex-col">
+      <h3 class="text-xl pb-2">Ingredients</h3>
+      <ul>
+        <li>Tomatoes</li>
+        <li>Tomatoes</li>
+        <li>Tomatoes</li>
+        <li>Tomatoes</li>
+      </ul>
+    </div>
+    <div class="grow w-full">
+      <h3 class="text-xl border-b-2 border-purple-300 w-full pb-2">Instructions</h3>
+      <ul class="pl-5">
+        <li class="py-3">Step 1</li>
+        <li class="py-3">Step 2</li>
+        <li class="py-3">Step 3</li>
+        <li class="py-3">Step 4</li>
+      </ul>
+    </div>
+  </div>
+
+</body>
+
+</html>
