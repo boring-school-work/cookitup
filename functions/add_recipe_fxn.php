1e9b011d282b977ae1c4c0f23a36b62c7eef5f45
Bl@ze <123462762+BlazingTwister@users.noreply.github.com>
2024-03-10 18:41:26 +0000

    New implementation
    Added implementation for storing actual image in the database

--- functions/add_recipe_fxn.php
+++ functions/add_recipe_fxn.php
@@ -4,7 +4,7 @@
 require_once("../settings/connection.php");
 
 // Check if form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   // Get form data
   $title = $_POST['title'];
@@ -20,8 +20,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           
   $stmt = $conn->prepare($sql)
 
+
   // Bind parameters
-  $stmt->bind_param("sssssss", $title, $description, $image, $ingredients, $directions, $userId)
+  $null = NULL; // So we can bind the image
+  $stmt->bind_param("ssbssss", $title, $description, $null, $ingredients, $directions, $userId)  //b indicates binary type
+
+
+  /* send_long_data allows sending data to the server in pieces
+   which is necessary for larger binary data like images.
+   */
+
+  // Set $null as the image data
+  $stmt->send_long_data(2, $image);   //store the actual image in database
 
   // Execute statement and check for success
   if ($stmt->execute()){
@@ -31,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Redirect to home page
     header("Location: ../view/home/index.php");
-    
+
   } 
   else {
     echo "Error adding recipe: " . mysqli_error($conn);
