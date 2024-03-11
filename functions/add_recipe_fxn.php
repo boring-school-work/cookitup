117aa61a1c52c97b833c253bd867e6b27b131410
David Saah <dasorange.hope@gmail.com>
2024-03-11 22:31:29 +0000

    fix: update function to save form data to recipe table

--- functions/add_recipe_fxn.php
+++ functions/add_recipe_fxn.php
@@ -1,7 +1,12 @@
 <?php
 
+session_start();
+
 // Include database connection 
 require_once("../settings/connection.php");
+
+// get upload function
+require_once("./upload.php");
 
 // Check if form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -9,50 +14,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Get form data
   $title = $_POST['title'];
   $description = $_POST['description'];
-  $image = $_POST['food-image']; 
+  $image =  $_FILES['food-image'];
   $ingredients = $_POST['ingredient'];
-  $directions = $_POST['directions'];
-  $userId = $_SESSION['user_id']; 
+  $instructions = $_POST['instructions'];
+  $cook_time = $_POST['cook-time'];
+
+  // get other data for database
+  $userId = $_SESSION['user_id'];
+  $image_path = upload_image($image);
 
   // Prepare SQL statement
-  $sql = "INSERT INTO recipes (title, description, image_url, ingredients, instructions, author_id) 
-          VALUES (?, ?, ?, ?, ?, ?)";
-          
-  $stmt = $conn->prepare($sql)
-
+  $sql = mysqli_prepare(
+    $conn,
+    "INSERT INTO recipes (title, description, image_url, cook_time, instructions, author_id) 
+            VALUES (?, ?, ?, ?, ?, ?)"
+  );
 
   // Bind parameters
-  $null = NULL; // So we can bind the image
-  $stmt->bind_param("ssbssss", $title, $description, $null, $ingredients, $directions, $userId)  //b indicates binary type
-
-
-  /* send_long_data allows sending data to the server in pieces
-   which is necessary for larger binary data like images.
-   */
-
-  // Set $null as the image data
-  $stmt->send_long_data(2, $image);   //store the actual image in database
+  mysqli_stmt_bind_param($sql, "ssssss", $title, $description, $image_path, $cook_time, $instructions, $userId);
 
   // Execute statement and check for success
-  if ($stmt->execute()){
-    
+  if (!mysqli_stmt_execute($sql)) {
+    die("Internal Server Error: Could not insert into database.");
+  } else {
     // Success message 
     echo "Recipe added successfully!";
-
-    // Redirect to home page
-    header("Location: ../view/home/index.php");
-
-  } 
-  else {
-    echo "Error adding recipe: " . mysqli_error($conn);
   }
 
-  // Close the prepared statement
-  $stmt->close();
+  // Close the connection
+  $conn->close();
 
-
-  exit();
+  // TODO: Redirect to post page
 }
-
-
-?>
