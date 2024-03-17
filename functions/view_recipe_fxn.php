155ef4985ea1446af5ce1d46add7313d0c132327
David Saah <dasorange.hope@gmail.com>
2024-03-17 13:10:12 +0000

    feat: add function to get recipe data from database

---
+++ functions/view_recipe_fxn.php
@@ -0,0 +1,29 @@
+<?php
+
+/** Gets the recipe data from the database
+ * Returns an array of the recipe data
+ *
+ * @param int $id
+ * @param mysqli $conn
+ * @return array
+ */
+function get_recipe_data($conn, $id)
+{
+  $result = $conn->query(
+    "
+    SELECT title, description, cook_time, image_url, instructions,
+    CONCAT(fname, ' ', lname) AS author_name
+    FROM recipes
+    INNER JOIN users
+    ON author_id=user_id
+    WHERE recipe_id =$id
+    "
+  );
+  $recipe = $result->fetch_assoc();
+  $ingredients = $conn->query("SELECT name FROM ingredients WHERE recipe_id=$id");
+  $recipe['ingredients'] = $ingredients;
+
+  $conn->close();
+
+  return $recipe;
+}
